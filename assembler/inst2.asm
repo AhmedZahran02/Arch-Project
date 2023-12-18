d77a61e567f77f91f7aca8d0a5c32e7e8056e4ca
Mohammed Adel Mohammed <107713170+MoAdelEzz@users.noreply.github.com>
2023-12-18 14:35:05 +0200

    assembler write memory files and tested

--- assembler/inst2.asm
+++ assembler/inst2.asm
@@ -1,12 +1,12 @@
 # all numbers in hex format
 # we always start by reset signal
 #this is a commented line
-.ORG 0  #this means the the following line would be  at address  0 , and this is the reset address
-10
+#.ORG 0  #this means the the following line would be  at address  0 , and this is the reset address
+#10
 #you should ignore empty lines
 
-.ORG 2  #this is the interrupt address
-100
+#.ORG 2  #this is the interrupt address
+#100
 
 .data
 20
